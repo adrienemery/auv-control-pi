e06e83917641fa9a5b5817e37c910249e2b55bb8
Adrien Emery <adrien.emery@gmail.com>
2018-11-07 01:41:59 +0000

    remove unused gps

--- auv_control_pi/management/commands/runauv.py
+++ auv_control_pi/management/commands/runauv.py
@@ -3,7 +3,7 @@ import logging
 from autobahn.asyncio.component import Component, run
 from django.core.management.base import BaseCommand
 from auv_control_pi.components.auv_control import AUV
-from auv_control_pi.components.gps import GPS
+#from auv_control_pi.components.gps import GPS
 
 logging.basicConfig(level=logging.INFO)
 
@@ -16,9 +16,9 @@ class Command(BaseCommand):
             realm="realm1",
             session_factory=AUV,
         )
-        gps_comp = Component(
-            transports="ws://crossbar:8080/ws",
-            realm="realm1",
-            session_factory=GPS,
-        )
-        run([auv_comp, gps_comp])
+ #       gps_comp = Component(
+ #           transports="ws://crossbar:8080/ws",
+ #           realm="realm1",
+ #           session_factory=GPS,
+ #       )
+        run([auv_comp])
