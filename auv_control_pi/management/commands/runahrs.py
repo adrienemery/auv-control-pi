b73e9ce3899db53b305390826d0085758095a8ef
Adrien Emery <adrien.emery@gmail.com>
2018-11-11 12:43:01 -0800

    Add ahrs to docker-compose

---
+++ auv_control_pi/management/commands/runahrs.py
@@ -0,0 +1,18 @@
+import logging
+
+from autobahn.asyncio.component import Component, run
+from django.core.management.base import BaseCommand
+from auv_control_pi.components.ahrs import AHRS
+
+logging.basicConfig(level=logging.INFO)
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        comp = Component(
+            transports="ws://crossbar:8080/ws",
+            realm="realm1",
+            session_factory=AHRS,
+        )
+        run([comp])
